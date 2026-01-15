f6884b99fad6012eda5abd444ea9d97b16f33465
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2026-01-15 13:58:48 +0000

    venn diagram demo

--- wc_demo.py
+++ wc_demo.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from wordcloud import WordCloud
+from collections import Counter
 
 sns.set_theme(style="ticks", palette="pastel")
 
@@ -15,16 +16,18 @@ print(df_colors.info())
 # format a series (column) from the dataframe to use
 wc_series = df_colors['Service']
 print(wc_series)
-service_list = str(wc_series.values)
+service_list = wc_series.values
 print(service_list)
-service_str = ' '.join(item for item in service_list.split())
-print(service_str)
+print(type(service_list))
+services = Counter(service_list)
+#service_str = ' '.join(item for item in service_list.split())
+#print(service_str)
 
 # .generate takes in a STRING like:
 sample_str = "penne fusilli rigatoni gnocchi spaghetti farfalle linguini bucatini lasagna stelline"
 
 # Create a wordcloud from that data
-wordcloud = WordCloud(width=800, height=400).generate(service_str)
+wordcloud = WordCloud(width=800, height=400).generate_from_frequencies(services)
 plt.figure(figsize=(10, 5))
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis('off')  
